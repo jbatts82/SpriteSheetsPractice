b5e3198588a6f5247332bf71cd2afb8d90943dcc
jbatts82 <jbatts82@hotmail.com>
2022-11-13 06:50:34 -0500

    Added individual action functionality.

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ pygame.init()
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
-BG = (50, 50, 50)
-BLACK = (0, 0, 0)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('SpriteSheets')
@@ -20,31 +18,55 @@ pygame.display.set_caption('SpriteSheets')
 sprite_sheet_image = pygame.image.load('doux.png').convert_alpha()
 sprite_sheet = spriteSheet.SpriteSheet(sprite_sheet_image)
 
+BG = (50, 50, 50)
+BLACK = (0, 0, 0)
 
-def get_image(sheet, frame,  width, height, scale, color):
-    image = pygame.Surface((width, height)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame * width), 0, width, height))
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(color)
-    return image
+# create animation list
+animation_list = []
+animation_steps = [4, 6, 3, 4]
+action = 0
+last_update = pygame.time.get_ticks()
+animation_cooldown = 750
+frame = 0
+step_counter = 0
 
-
-frame_0 = sprite_sheet.get_image(0, 24, 24, 6, BLACK)
-frame_1 = sprite_sheet.get_image(1, 24, 24, 6, BLACK)
+for animation in animation_steps:
+    temp_img_list = []
+    for _ in range(animation):
+        temp_img_list.append(sprite_sheet.get_image(step_counter, 24, 24, 6, BLACK))
+        step_counter += 1
+    animation_list.append(temp_img_list)
 
 run = True
 
 if __name__ == '__main__':
     while run:
+
+        # update background
         screen.fill(BG)
 
-        # show frame image
-        screen.blit(frame_0, (0, 0))
-        screen.blit(frame_1, (100, 0))
+        # update animation
+        current_time = pygame.time.get_ticks()
+        if current_time - last_update >= animation_cooldown:
+            frame += 1
+            last_update = current_time
+            if frame >= len(animation_list[action]):
+                frame = 0
 
+        # show frame image
+        screen.blit(animation_list[action][frame], (0, 0))
+
+        # event Handler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN and action > 0:
+                    action -= 1
+                    frame = 0
+                if event.key == pygame.K_UP and action < len(animation_list) - 1:
+                    action += 1
+                    frame = 0
 
         pygame.display.update()
 
